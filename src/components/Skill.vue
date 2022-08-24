111ff4b90afa4f2c0497dd02f9c84633ad4a68ee
IndraDheepaBayu <indradheepa00@gmail.com>
2022-08-24 07:20:02 +0700

    vue-project

---
+++ src/components/Skill.vue
@@ -0,0 +1,117 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <div class="div">
+      <div class="flex">
+        <h3>HTML</h3>
+        <h3>85%</h3>
+      </div>
+      <div class="skills_bar">
+        <span class="skills_percetage skills_html"></span>
+      </div>
+    </div>  
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <div class="div">
+      <div class="flex">
+        <h3>CSS</h3>
+        <h3>85%</h3>
+      </div>
+      <div class="skills_bar">
+        <span class="skills_percetage skills_css"></span>
+      </div>
+    </div>  
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <div class="div">
+      <div class="flex">
+        <h3>FIGMA</h3>
+        <h3>80%</h3>
+      </div>
+      <div class="skills_bar">
+        <span class="skills_percetage skills_figma"></span>
+      </div>
+    </div>   
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <div class="div">
+      <div class="flex">
+        <h3>JAVA SCRIPT</h3>
+        <h3>70%</h3>
+      </div>
+      <div class="skills_bar">
+        <span class="skills_percetage skills_js"></span>
+      </div>
+    </div>  
+  </WelcomeItem>
+</template>
+
+<style>
+.div{
+  height: 85px;
+}
+
+.flex{
+  display: flex;
+  justify-content: space-between;
+}
+
+h3{
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+.skills_bar,
+.skills_percetage{
+  margin-top: 5px;
+  height: 5px;
+  border-radius: .25rem;
+}
+
+.skills_bar{
+  background-color: #fff;
+}
+
+.skills_percetage{
+  display: block;
+  background-color: hsla(160, 100%, 37%, 1);
+}
+
+.skills_html{
+  width: 85%;
+}
+.skills_css{
+  width: 85%;
+}
+.skills_figma{
+  width: 80%;
+}
+
+.skills_js{
+  width: 70%;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,68 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Personal Profile</template>
+    <p>Hello</p>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Education</template>
+    <h4>2020-2023</h4>
+    <a href="http://portal.smktarunabhakti.net/login/index.php" target="_blank">SMK TARUNA BHAKTI</a>
+    <h4>
+      Web Developer
+    </h4>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Experience</template>
+    <ul>
+      <li>Scholl Agenda Guru</li>
+      <li>Food Delivery Design</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Language</template>
+    <ul>
+      <li>English</li>
+      <li>Bahasa Indonesia</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Contact</template>
+    <h4>
+      (+62) 819-9779-0540
+    </h4>
+    <h4>
+      indradheepa00@gmail.com
+    </h4>
+    <h4>
+      <a href="https://github.com/IndraDheepaBayu" target="_blank">Indra GitHub</a>
+    </h4>
+  </WelcomeItem>
+</template>
